b5dc95b9e4d0af6ba432029e2ea660d241b07c3d
Luke Jones <nnblc565@gmail.com>
2021-12-20 13:05:49 +1030

    Commenting to remind me of the trial

--- Source/MessAround/Private/ProgressorInclude.h
+++ Source/MessAround/Private/ProgressorInclude.h
@@ -11,7 +11,9 @@ enum EStageType {
 	END = 1  UMETA(DisplayName = "END")
 };
 
-
+/// <summary>
+/// IN ORDER TO DEFINE DELEGATES IN A HEADER FILES FOR INCLUSION IN OTHER FILES. MAKE SURE THERE IS A UNREAL CLASS, STRUCT or ENUM INSIDE THE FILE!
+/// </summary>
 DECLARE_DYNAMIC_DELEGATE(FStageEnterExit);
 DECLARE_DYNAMIC_DELEGATE_OneParam(FStageTick, float, DeltaTime);
 DECLARE_DYNAMIC_DELEGATE_TwoParams(FStepStateChange, bool, StepCompletedState, int32, Step);
